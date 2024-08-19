bb4d8005f40c018f805e7763a28cacf76d4e1374
root <marksmith3981274@gmail.com>
2024-08-19 19:11:08 -0400

    Edit apply.yml, index.js; little changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('.. ..2 UPDATED !!!!! ..');
+  res.write('.. ..2 PLS MAN5 !!!!! ..');
   res.end();
 }).listen(8080);
